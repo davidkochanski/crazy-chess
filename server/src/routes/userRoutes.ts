1baef7ff55c7d53fef86fc9a73b2cd092e69e752
davidkochanski <kochanskidavid9@gmail.com>
2024-08-21 15:48:41 -0400

    Add (testing) PATCH endpoint for setting a user's name

--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
-import userHandler from "../controllers/userController";
+import { setNameHandler } from "../controllers/userController";
+import { userHandler } from "../controllers/userController";
 
 const userRoutes = Router();
 
 userRoutes.get("/", userHandler); // NO /user in the first param.
                                   // we already put /user in the index.ts file.
                                   // so this is just /user, NOT /user/user.
+
+userRoutes.patch("/setName", setNameHandler)
 
 export default userRoutes;
